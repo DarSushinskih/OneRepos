54443975c2cb7283508e61df3205e2ff66902ecb
DarSushinskih <sushinskih.darya@yandex.ru>
2019-05-23 19:26:06 +0500

    test

---
+++ Test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Открой замок!");
+            Console.WriteLine("Для того что бы подобрать замок, вы должы подоборать числа только к остальным значениям ячеек!");
+            Random rnd = new Random();                                                                              //1 
+            Dictionary<int, int> Castle = new Dictionary<int, int> { { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 },        //2
+                                { 5, 0 }, { 6, 0 }, { 7, 0 }, { 8, 0 }, { 9, 0 }, { 10, 0 }};
+            Castle[rnd.Next(1, 11)] = rnd.Next(1, 7);
+            Castle[rnd.Next(1, 11)] = rnd.Next(1, 7);
+
+            foreach (var x in Castle)                                                                           //3
+            {
+                Console.WriteLine(x.Key.ToString() + ". = " + x.Value.ToString());                              //4 
+            }
+
+            Console.WriteLine("Теперь по очерёдно вводите значения ячеек от 1 до 6:");
+                
+            
+            for (int i = 1; i < Castle.Count + 1; i++)                                                       //5 
+            {
+            vvod:                                                                                  //6
+            if (Castle[i] != 0) continue;
+            Console.WriteLine("Введите значение для ячейки №" + i + ":");                         
+            int input = -1;
+            try                                                                                  //7
+            {
+                  input = Convert.ToInt32(Console.ReadLine());                                    //8
+            }
+            catch (Exception e)                                                                 //9
+            {
+            Console.WriteLine("Не верно введено значение!");                                //10
+            goto vvod;
+            }
+            if (input < 1 || input >= 7)                                                        //11
+            {
+            Console.WriteLine("Не верно введено значение!");                                //12
+            goto vvod;
+            }
+            Castle[i] = input;                                                               //13
+            }
+              
+            Console.WriteLine("Замок имеет вид: ");
+
+            foreach (var x in Castle)                                                           //14
+            {
+                Console.WriteLine("№" + x.Key + " = " + x.Value);                               //15
+            }
+
+
+            if (SeachWin(Castle))                                                               //16
+            {
+                Console.WriteLine("Поздравляю! Вы открыли замок!");                             //17
+            }
+            else                                                                                //18
+            {
+                Console.WriteLine("Увы! В другой раз =(");                                      //19
+            }
+
+            Console.ReadLine();                                                                 //20                                                          
+        }                                                           
+
+        public static bool SeachWin(Dictionary<int, int> castle)                              //21
+        {
+            int result = 0;                                                                     //22
+            for (int i = 3; i < 11; i++)                                                        //23
+            {
+                result = castle[i] + castle[i - 1] + castle[i - 2];                                //24-26
+                if (result == 10) return true;      
+                else result = 0;
+            }
+            return false; 
+        }
+    }
+}
---
+++ Test/ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void test_search_win_default()
+        {
+            Dictionary<int, int> Castle = new Dictionary<int, int> { { 1, 5 }, { 2, 3 }, { 3, 4}, { 4, 1 },
+                { 5, 3 }, { 6, 4 }, { 7, 2 }, { 8, 1 }, { 9, 4 }, { 10, 5 }};
+
+            Assert.AreEqual(true, Program.SeachWin(Castle));
+        }
+        [TestMethod]
+        public void test_search_win_failure()
+        {
+            Dictionary<int, int> Castle = new Dictionary<int, int> { { 1, 5 }, { 2, 3 }, { 3, 4}, { 4, 1 },
+                                { 5, 3 }, { 6, 4 }, { 7, 2 }, { 8, 1 }, { 9, 4 }, { 10, 5 }};
+
+            Assert.AreEqual(true, Program.SeachWin(Castle));
+        }
+        [TestMethod]
+        public void test_search_win_feilure1()
+        {
+            Dictionary<int, int> Castle = new Dictionary<int, int> { { 1, 5 }, { 2, 5 }, { 3, 5}, { 4, 5 }, 
+                                { 5, 5 }, { 6, 5 }, { 7, 5 }, { 8, 5 }, { 9, 5}, { 10, 5 }};
+
+            Assert.AreEqual(false, Program.SeachWin(Castle));
+        }
+        [TestMethod]
+        public void test_search_win_default1()
+        {
+            Dictionary<int, int> Castle = new Dictionary<int, int> { { 1, 5 }, { 2, 1 }, { 3, 4}, { 4, 1 },
+                                { 5, 3 }, { 6, 4 }, { 7, 2 }, { 8, 1 }, { 9, 5 }, { 10, 5 }};
+
+            Assert.AreEqual(true, Program.SeachWin(Castle));
+        }
+        [TestMethod]
+        public void test_search_win_default2()
+        {
+            Dictionary<int, int> Castle = new Dictionary<int, int> { { 1, 1 }, { 2, 2 }, { 3, 4}, { 4, 4 },
+                                { 5, 3 }, { 6, 4 }, { 7, 2 }, { 8, 1 }, { 9, 1 }, { 10, 5 }};
+
+            Assert.AreEqual(true, Program.SeachWin(Castle));
+        }
+        [TestMethod]
+        public void test_search_win_default3()
+        {
+            Dictionary<int, int> Castle = new Dictionary<int, int> { { 1, 1 }, { 2, 1 }, { 3, 4}, { 4, 4 },
+                                { 5, 2 }, { 6, 4 }, { 7, 2 }, { 8, 1 }, { 9, 1 }, { 10, 5 }};
+            Assert.AreEqual(true, Program.SeachWin(Castle));
+        }
+        [TestMethod]
+        public void test_search_win_default4()
+        {
+            Dictionary<int, int> Castle = new Dictionary<int, int> { { 1, 1 }, { 2, 1 }, { 3, 3}, { 4, 4 },
+                                { 5, 2 }, { 6, 4 }, { 7, 2 }, { 8, 1 }, { 9, 1 }, { 10, 5 }};
+            Assert.AreEqual(true, Program.SeachWin(Castle));
+        }
+        [TestMethod]
+        public void test_search_win_default5()
+        {
+            Dictionary<int, int> Castle = new Dictionary<int, int> { { 1, 6 }, { 2, 6 }, { 3, 6}, { 4, 6 },
+                                { 5, 6 }, { 6, 6 }, { 7, 6 }, { 8, 6 }, { 9, 6 }, { 10, 6 }};
+            Assert.AreEqual(false, Program.SeachWin(Castle));
+        }
+        [TestMethod]
+        public void test_search_win_default7()
+        {
+            Dictionary<int, int> Castle = new Dictionary<int, int> { { 1, 1 }, { 2, 1 }, { 3, 1 }, { 4, 1 },
+                                { 5, 1 }, { 6, 1 }, { 7, 1}, { 8, 1 }, { 9, 1 }, { 10, 1 }};
+            Assert.AreEqual(false, Program.SeachWin(Castle));
+        }
+    }
+}
