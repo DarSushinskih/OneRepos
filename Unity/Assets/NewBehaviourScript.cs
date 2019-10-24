01ea40d461bc92aed464249d30b58485e69f269a
DarSushinskih <sushinskih.darya@yandex.ru>
2019-10-24 17:57:18 +0500

    interactions

--- Unity/Assets/NewBehaviourScript.cs
+++ Unity/Assets/NewBehaviourScript.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadScene(int Level)
     {
-        
+        SceneManager.LoadScene(Level);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        Application.Quit ();
     }
 }
+
