14dadc113f4fed4156940098503e166cd93e4bad
DarSushinskih <sushinskih.darya@yandex.ru>
2019-11-26 17:24:52 +0500

    level 1

---
+++ Unity/Assets/script/level1.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class level1 : MonoBehaviour
+{
+
+    [SerializeField] Text otvet, prov;
+    [SerializeField] Text[] text = new Text[8];
+    [SerializeField] int[] result = new int[6];
+    public GameObject image;
+    public GameObject image1;
+
+    private void Start()
+    {
+        Rnd();
+        image.SetActive(false);
+        image1.SetActive(false);
+    }
+    public void Rnd()
+    {
+        text[3].text = (Random.Range(1, 6)).ToString();
+        text[5].text = (Random.Range(1, 6)).ToString();
+    }
+    public void Summa()
+    {
+
+        for(int i = 0; i <= 5; i++)
+        {
+            result[i] = (int.Parse(text[i].text) + int.Parse(text[i + 1].text) + int.Parse(text[i + 2].text));
+        }
+    }
+    public void Vvod()
+    {
+        for (int i = 0; i <= 7; i++)
+        {
+            if (0 > int.Parse(text[i].text) || int.Parse(text[i].text) >= 6)
+            {
+                prov.text = "Введите число из допустимого диапазона!!!";
+                return;
+            }
+        }
+    }
+    public void Proverka()
+    {
+        Rnd();
+        Vvod();
+        Summa();
+        
+        if (result[0] == 10 | result[1] == 10 | result[2] == 10 | result[3] == 10 | result[4] == 10 | result[5] == 10)
+        {
+            image1.SetActive(true);
+        }
+        else
+        {
+            image.SetActive(true);
+        }
+    }
+    public void Knopka()
+    {
+        image.SetActive(false);
+    }
+   
+}
---
+++ Unity/Assets/script/level2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class level2 : MonoBehaviour
+{
+    [SerializeField] Text[] text = new Text[9];
+
+    void Start()
+    {
+       
+
+        text[0].text = (Random.Range(0, 9)).ToString();
+        text[1].text = (Random.Range(0, 9)).ToString();
+        text[2].text = (Random.Range(0, 9)).ToString();
+
+        text[3].text = (Random.Range(0, 9)).ToString();
+        text[4].text = (Random.Range(0, 9)).ToString();
+        text[5].text = (Random.Range(0, 9)).ToString();
+
+        text[6].text = text[2].text;
+        text[7].text = text[5].text;
+        text[8].text = (Random.Range(0, 9)).ToString();
+
+        text[9].text = (Random.Range(0, 9)).ToString();
+        text[10].text = (Random.Range(0, 9)).ToString();
+        text[11].text = (Random.Range(0, 9)).ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
